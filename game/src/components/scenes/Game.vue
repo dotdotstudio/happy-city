176730945c638d847d7f9283dbaa0ccb0b0b14ca
drcheese <k.a.stevenson@gmail.com>
2022-06-23 18:03:29 -0400

    Copy update

--- game/src/components/scenes/Game.vue
+++ game/src/components/scenes/Game.vue
@@ -55,7 +55,7 @@
       <div class="back-button">
         <push-button class="green space-font-mono" narrow @click="restartGame()">
           <span><icon name="reply"></icon></span>
-          Retry
+          Play Again
         </push-button>
         <push-button class="orange space-font-mono" narrow @click="goToMenu()">
           <span><icon name="frown-o"></icon></span>
